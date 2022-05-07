62b38601a83f7d999690b60bb7b6d409215348c9
Xchkoo <Xchkoo@foxmail.com>
2022-05-07 09:33:18 +0800

    alpha version all set

---
+++ src/plugins/help/__init__.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from nonebot.matcher import Matcher
+from nonebot.permission import SUPERUSER
+from nonebot import on_notice, get_driver, on_command, on_message
+from nonebot import get_driver
+from nonebot.rule import to_me
+from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters.onebot.v11.message import Message
+
+
+global_config = get_driver().config
+# status_config = Config(**global_config.dict())
+
+help = on_command(cmd="帮助", aliases={'help'}, rule=to_me(), priority=10)
+
+@help.handle()
+async def help_handle(bot: Bot, event: Event):
+    data = \
+    f"""
+    [CQ:at,qq={event.get_user_id()}]: 欢迎使用Valorant_bot!
+    ***此机器人 Valorant_bot (简称vb) 正在早期开发中，bug反馈联系qq1524049410。***
+    
+    
+    使用方式： @vb并使用以下命令 有的命令前需加上'/'或'!'。
+    普通指令：
+    - 帮助
+        用法：*可通过qq戳一戳使用此命令*
+             @vb /帮助 | help
+        说明：显示可用的命令
+    - *开发中* 开黑 
+        用法：@vb /开黑 | kaihei | kh <开黑公告>
+        会自动在机器人加入的群组内转发
+        例：
+        @vb /开黑 3等2 白金局 来能躺的 游戏id：XXX #xxx 开黑啦服务器：XXXXXXXX
+
+
+    管理员指令：
+    - *开发中* 添加群组
+        用法：@vb /添加群组 | add_group <群组id>
+        说明：添加开黑公告通知群组
+    - *开发中* 删除群组
+        用法：@vb /删除群组 | del_group <群组id>
+        说明：删除开黑公告通知群组
+    - *开发中* 显示群组
+        用法：@vb /显示群组 | show_group
+        说明：显示群组
+
+
+    一些无聊的命令：
+    - 人生重开 
+        用法：@vb /人生重开 | remake | liferestart | 人生重来
+        说明：人生重开
+    - 疯狂星期四
+        用法：疯狂星期四
+        说明：疯狂星期四
+    - 随机唐可可
+        用法：/随机唐可可|鲤鱼|鲤鱼王|Liyuu|liyuu <难度：简单|普通|困难|地狱>
+        说明：来找到唐可可吧
+    - 每日一句
+        用法：每日一句
+        说明：放点洋屁
+
+    """
+    await help.finish(Message(data))
+
+
+try:
+    from nonebot.adapters.onebot.v11 import PokeNotifyEvent, PrivateMessageEvent
+except ImportError:
+    pass
+else:
+
+    async def _group_poke(event: PokeNotifyEvent) -> bool:
+        return event.is_tome()
+
+    group_poke = on_notice(_group_poke, priority=10, block=True)
+    group_poke.handle()(help_handle)
+
+    async def _poke(event: PrivateMessageEvent) -> bool:
+        return event.sub_type == "friend" and event.message[0].type == "poke"
+
+    poke = on_message(
+        _poke,
+        priority=10,
+    )
+    poke.handle()(help_handle)
+
---
+++ src/plugins/kaihei/__init__.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from nonebot.matcher import Matcher
+from nonebot.permission import SUPERUSER
+from nonebot import get_driver, on_command
+from nonebot.plugin import require
+from nonebot.rule import to_me
+from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters.onebot.v11.message import Message
+from nonebot.typing import T_State
+from nonebot.params import Arg, Depends, CommandArg
+from .config import Config
+import traceback
+from sqlite3 import IntegrityError
+
+export = require("nonebot_plugin_navicat")
+
+global_config = get_driver().config
+plugin_config = Config.parse_obj(get_driver().config)
+
+kaihei = on_command(
+    cmd="开黑", rule=to_me(), aliases={'kaihei', 'kh'}, priority=10, block=True
+)
+
+
+@kaihei.handle()
+async def handle_first_receive(state: T_State, args: Message = CommandArg()):
+    plaintext = args.extract_plain_text()
+    if plaintext:
+        state["kaihei_text"] = plaintext
+
+
+@kaihei.got("kaihei_text", prompt="请输入广播内容：")
+async def handle_group_message(event: Event, bot: Bot, state: T_State, kaihei_text: str = Arg()):
+    data = []
+    msg = Message(f'[CQ:at,qq={event.get_user_id()}]' + "向全体瓦罗兰特玩家公告：\n") + kaihei_text
+    try:
+        query = "SELECT * FROM QQGROUP"
+        rows = await export.sqlite_pool.fetch_all(query=query)
+        for col in rows:
+            group_id = col[1]
+            data.append("\n" + str(group_id))
+            await bot.send_group_msg(group_id=group_id, message=msg)
+    except:
+        await add_group.finish("ERROR: 数据库出现问题")
+    await kaihei.finish("公告完毕!\n\n公告内容：\n" + msg + "\n\n公告群组：" + data)
+
+
+add_group = on_command(
+    cmd="添加群组", rule=to_me(), aliases={'add_group'}, permission=SUPERUSER, priority=2, block=True
+)
+
+
+def parse_int(key: str):
+    """解析数字，并将结果存入 state 中"""
+
+    async def _key_parser(
+            matcher: Matcher, state: T_State, a_input=Arg(key)
+    ):
+        print(f"运行 parse_int, key: {key}")
+        if isinstance(a_input, int):
+            return
+
+        plaintext = input.extract_plain_text()
+        if not plaintext.isdigit():
+            await matcher.reject_arg(key, "请只输入数字，不然我没法理解呢！")
+        state[key] = int(plaintext)
+
+    return _key_parser
+
+
+@add_group.handle()
+async def handle_first_receive(state: T_State, args: Message = CommandArg()):
+    plaintext = args.extract_plain_text()
+    if plaintext.isdigit():
+        state["group_id"] = int(plaintext)
+
+
+@add_group.got("group_id", prompt="请输入一个群组id：", parameterless=[Depends(parse_int("group_id"))])
+async def handle_group_message(bot: Bot, group_id: int = Arg()):
+    await export.sqlite_pool.connect()
+    try:
+        query = "INSERT INTO QQGROUP(GROUP_ID) VALUES (:GROUP_ID)"
+        values = {"GROUP_ID": group_id}
+        await export.sqlite_pool.execute(query=query, values=values)
+    except IntegrityError:
+        traceback.print_exc()
+        await add_group.finish("ERROR: 出现重复群组")
+    except:
+        await add_group.finish("ERROR: 数据库出现问题")
+    await bot.send_group_msg(group_id=group_id, message="此群已被添加到Valorant_bot开黑叫人服务 若有错误 请管理员联系1524049410")
+    await add_group.finish("添加" + str(group_id) + "完毕")
+
+
+del_group = on_command(cmd="删除群组", aliases={'del_group'}, permission=SUPERUSER, priority=10, block=False)
+
+
+@del_group.handle()
+async def handle_first_receive(state: T_State, args: Message = CommandArg()):
+    plaintext = args.extract_plain_text()
+    if plaintext.isdigit():
+        state["group_id"] = int(plaintext)
+
+
+@del_group.got("group_id", prompt="请输入一个群组id：", parameterless=[Depends(parse_int("group_id"))])
+async def handle_group_message(bot: Bot, group_id: int = Arg()):
+    await export.sqlite_pool.connect()
+    try:
+        query = "DELETE FROM QQGROUP WHERE GROUP_ID=:GROUP_ID"
+        values = {"GROUP_ID": group_id}
+        await export.sqlite_pool.execute(query=query, values=values)
+        try:
+            await bot.send_group_msg(group_id=group_id, message="此群已从Valorant_bot开黑叫人服务中删除 若有错误 请管理员联系1524049410")
+        except:
+            traceback.print_exc()
+            await del_group.finish
+    except IntegrityError:
+        traceback.print_exc()
+        await del_group.finish("ERROR: 数据库中无需删除群组")
+    except:
+        await del_group.finish("ERROR: 数据库出现问题")
+
+    await del_group.finish("删除" + str(group_id) + "完毕")
+
+
+show_group = on_command(cmd="显示群组", aliases={'show_group'}, permission=SUPERUSER, priority=10, block=False)
+
+
+@show_group.handle()
+async def handle_show_group():
+    data = []
+    try:
+        query = "SELECT * FROM QQGROUP"
+        rows = await export.sqlite_pool.fetch_all(query=query)
+        for col in rows:
+            group_id = col[1]
+            data.append("群id：" + str(group_id) + '\n')
+    except:
+        traceback.print_exc()
+    await show_group.send(message=Message(data))
+    await show_group.finish("报告完毕")
